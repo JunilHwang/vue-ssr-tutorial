4f0ab0090175c19f3f00aa1a53567475b9790db1
junil-hwang <tjsdlf4261@naver.com>
2020-04-15 20:34:07 +0900

    vue + nest ssr 성공

--- vue-nest-ssr/back-end/app.controller.ts
+++ vue-nest-ssr/back-end/app.controller.ts
@@ -1,4 +1,4 @@
-import {Controller, Get, Req, Res} from '@nestjs/common';
+import {Controller, Get, Render, Req, Res} from '@nestjs/common';
 import { AppService } from './app.service';
 import {Request, Response} from "express";
 
@@ -7,7 +7,10 @@ export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get('*')
-  async getHome(@Req() req: Request, @Res() res: Response): Promise<void> {
-    res.end(await this.appService.getSSRTemplate({ url: req.url }))
+  @Render('index')
+  async getHome(@Req() req: Request, @Res() res: Response): Promise<{ [k: string]: string }> {
+    const content: string = await this.appService.getSSRTemplate({ url: req.url })
+    const title: string = 'SSR'
+    return { content, title }
   }
 }
--- vue-nest-ssr/back-end/app.service.ts
+++ vue-nest-ssr/back-end/app.service.ts
@@ -1,12 +1,11 @@
 import { Injectable } from '@nestjs/common';
 import { createBundleRenderer } from 'vue-server-renderer'
-import path from 'path'
-import fs from 'fs'
+import { join } from 'path'
 
 @Injectable()
 export class AppService {
   getSSRTemplate(context: object): Promise<string> {
-    const renderer = createBundleRenderer(path.join(__dirname, '../static/vue-ssr-server-bundle.json'), {
+    const renderer = createBundleRenderer(join(__dirname, '../static/vue-ssr-server-bundle.json'), {
       runInNewContext: false
     })
     return renderer.renderToString(context)
