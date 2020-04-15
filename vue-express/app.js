1cbeaeef864c19c001e4574d9313c3e6efa864ae
junil-hwang <tjsdlf4261@naver.com>
2020-04-15 14:47:13 +0900

    폴더 구조 변경

--- vue-express/app.js
+++ vue-express/app.js
@@ -5,7 +5,7 @@ const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const sassMiddleware = require('node-sass-middleware');
 
-const indexRouter = require('./routes/index');
+const indexRouter = require('./routes');
 const usersRouter = require('./routes/users');
 const ssrRouter = require('./routes/ssr');
 
