3346d05b117172d44fe3769f9c5b3f139134df69
junil-hwang <tjsdlf4261@naver.com>
2020-04-15 21:16:05 +0900

    Vue + Nest + SSR 완성도 90%

--- vue-nest-ssr/back-end/main.ts
+++ vue-nest-ssr/back-end/main.ts
@@ -2,12 +2,19 @@ import { NestFactory } from '@nestjs/core';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { join } from 'path';
 import { AppModule } from './app.module';
+import express from 'express'
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
-  app.useStaticAssets(join(__dirname, '../resources/static'));
-  app.setBaseViewsDir(join(__dirname, '../resources/template'));
+  const staticPath: string = join(__dirname, '../resources/static')
+  // app.useStaticAssets(staticPath);
+  app.setBaseViewsDir(join(__dirname, '../resources/templates'));
   app.setViewEngine('hbs');
+  app.use('/static', express.static(staticPath, {
+    cacheControl: true,
+    maxAge: 3600 * 1000,
+    etag: false
+  }));
   await app.listen(3000);
 }
 bootstrap();
--- vue-nest-ssr/vue.config.js
+++ vue-nest-ssr/vue.config.js
@@ -8,18 +8,18 @@ module.exports = {
   productionSourceMap: false,
   outputDir: './resources',
   assetsDir: 'static',
-  pages: {
-    index: {
-      entry: `front-end/main${isDev ? '' : '-ssr'}.ts`,
-      template: isDev ? 'resources/stub/index.html' : 'resources/templates/index.hbs',
-      filename: 'resources/templates/dist/index.hbs', // 빌드 후 템플릿 파일
-      path: ['/*'] // dev 모드 프록시에 적용할 path
-    },
-  },
+  // pages: {
+  //   index: {
+  //     entry: `front-end/main${isDev ? '' : '-ssr'}.ts`,
+  //     template: isDev ? 'resources/stub/index.html' : 'resources/templates/index.hbs',
+  //     filename: 'resources/templates/dist/index.hbs', // 빌드 후 템플릿 파일
+  //     path: ['/*'] // dev 모드 프록시에 적용할 path
+  //   },
+  // },
 
   chainWebpack: config => {
     config.resolve.alias.set('@', path.join(__dirname, './front-end'))
-    // config.entry('app').clear().add(`./front-end/main${ isSSR ? '-ssr' : '' }.ts`).end()
+    config.entry('app').clear().add(`./front-end/main${ isSSR ? '-ssr' : '' }.ts`).end()
 
     if (isSSR) {
       config.target('node');
